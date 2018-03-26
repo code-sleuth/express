2ae04fa84e46d33279fdea2f1a8af2bc9bad79be
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-03-27 00:14:04 +0300

    [Chore] express get request

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let app = express()
 
 let port = 5000
 
+app.get('/', function(req, res){
+  res.send("Instantiated...")
+});
+
 app.listen(port, function(err){
   console.log('running server on port ' + port)
 });
