39acc0447667c58d4994de32992405b7fdd0d247
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-04-01 23:48:26 +0300

    [Chore] Setup jade templating engine

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let port = process.env.PORT || 5000;
 //use public folder to host static files
 app.use(express.static('public'));
 //host html files in views folder
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
     res.send('Instantiated...');
